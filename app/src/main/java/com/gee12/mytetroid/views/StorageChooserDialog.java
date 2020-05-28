abbc1aa3b420e28f7cf3779a051d9dbf53b1ce2b
gee12 <yagee12@gmail.com>
2020-05-28 07:21:46 +0300

    Add showing StorageChooser ListView on app first start.

--- app/src/main/java/com/gee12/mytetroid/views/StorageChooserDialog.java
+++ app/src/main/java/com/gee12/mytetroid/views/StorageChooserDialog.java
@@ -7,33 +7,30 @@ import androidx.appcompat.app.AlertDialog;
 
 import com.gee12.htmlwysiwygeditor.Dialogs;
 import com.gee12.mytetroid.R;
-import com.gee12.mytetroid.adapters.IntentsAdapter;
-import com.gee12.mytetroid.model.ReceivedData;
+import com.gee12.mytetroid.adapters.StorageChooserAdapter;
 
-public class IntentDialog {
+public class StorageChooserDialog {
 
     public interface IItemClickListener {
-        void onItemClick(ReceivedData item);
+        void onItemClick(boolean isNew);
     }
 
     /**
-     * Диалог со списком вариантов обработки переданного объекта.
+     * Диалог со списком вариантов.
      * @param context
-     * @param isText
      * @param listener
      */
-    public static void createDialog(Context context, boolean isText, IItemClickListener listener) {
+    public static void createDialog(Context context, IItemClickListener listener) {
         Dialogs.AskDialogBuilder builder = Dialogs.AskDialogBuilder.create(context, R.layout.dialog_list_view);
 //        builder.setTitle("Выберите действие");
         final AlertDialog dialog = builder.create();
 
         ListView listView = builder.getView().findViewById(R.id.list_view);
-        ReceivedData[] dataSet = (isText) ? ReceivedData.textIntents() : ReceivedData.imageIntents();
         listView.setOnItemClickListener((parent, view, position, id) -> {
-            listener.onItemClick(dataSet[position]);
+            listener.onItemClick(position == 1);
             dialog.cancel();
         });
-        listView.setAdapter(new IntentsAdapter(context, dataSet));
+        listView.setAdapter(new StorageChooserAdapter(context));
         dialog.show();
     }
 }
