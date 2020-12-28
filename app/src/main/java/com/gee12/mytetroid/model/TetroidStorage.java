ceb7a91c3f5f3582acb0264221ed69cabed887b8
gee12 <yagee12@gmail.com>
2020-12-28 22:02:50 +0300

    Add new fields to TetroidStorage.

--- app/src/main/java/com/gee12/mytetroid/model/TetroidStorage.java
+++ app/src/main/java/com/gee12/mytetroid/model/TetroidStorage.java
@@ -1,13 +1,81 @@
 package com.gee12.mytetroid.model;
 
+import com.gee12.mytetroid.data.TetroidXml;
+import com.gee12.mytetroid.data.crypt.TetroidCrypter;
+import com.gee12.mytetroid.data.ini.DatabaseConfig;
+
 import java.util.Date;
+import java.util.List;
 
 public class TetroidStorage {
 
+    // хранилище
+    private boolean mIsMain;
+    private int mOrder;
     private String mName;
     private String mPath;
-    private Date mCreated;
-    private Date mEdited;
+//    private boolean mIsUseTrash;
+    private String mTrashPath;
+    private Date mCreatedDate;
+    private Date mEditedDate;
+    private boolean mIsReadOnly;
+    private String mQuickNodeId;
+    private boolean mIsLoadFavoritesOnly;
+    private boolean mIsKeepLastNode;
+    private String mLastNodeId;
+
+    // шифрование
+    private boolean mIsSavePassHash;
+    private String mMiddlePassHash;
+    private boolean mIsRequestPIN;
+    private boolean mPINLength;
+    private String mPINHash;
+    private int mWhenAskPass;
+
+    // синхронизация
+    // ...
+    private int mIsCheckOutsideChanging;
+
+    // редактирование
+    private int mIsRecordEditMode;
+    private int mIsRecordAutoSave;
+    private int mIsFixEmptyParagraphs;
+
+    // отображение
+    private int mIsKeepScreenOn;
+    private int mIsDoubleTapFullscreen;
+    private int mIsShowRecordFields;
+    private int mIsHighlightRecordWithAttach;
+    private int mIsHighlightEncryptedNodes;
+    private int mHighlightColor;
+    private String mDateFormatString;
+    private List<Integer> mRecordFieldsInList;
+
+    private int mIsWriteLogToFile;
+    private String mLogPath;
+    //
+    /**
+     * Ветка для быстрой вставки.
+     */
+    protected TetroidNode mQuicklyNode;
+
+    /**
+     *
+     */
+    protected TetroidCrypter mCrypter;
+
+    /**
+     *
+     */
+    protected DatabaseConfig mDatabaseConfig;
+
+    protected TetroidXml mXml;
+
+    // временное
+    boolean isInited;
+    boolean isLoaded;
+    boolean isDecrypted;
+    boolean isCrypted;
 
     public TetroidStorage(String path) {
         Date curDate = new Date();
@@ -21,8 +89,8 @@ public class TetroidStorage {
     private void init(String name, String path, Date created, Date edited) {
         this.mName = name;
         this.mPath = path;
-        this.mCreated = created;
-        this.mEdited = edited;
+        this.mCreatedDate = created;
+        this.mEditedDate = edited;
     }
 
     public static String getFolderNameFromPath(String path) {
