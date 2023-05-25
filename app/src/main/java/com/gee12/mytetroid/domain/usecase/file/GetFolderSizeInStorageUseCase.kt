c108cf7dc80e91026658aeca01e72f30fa66491a
gee12 <yagee12@gmail.com>
2023-05-25 23:03:04 +0300

    Fixed calculating storage folder size

--- app/src/main/java/com/gee12/mytetroid/domain/usecase/file/GetFolderSizeInStorageUseCase.kt
+++ app/src/main/java/com/gee12/mytetroid/domain/usecase/file/GetFolderSizeInStorageUseCase.kt
@@ -1,9 +1,10 @@
 package com.gee12.mytetroid.domain.usecase.file
 
 import android.content.Context
+import android.text.format.Formatter
+import androidx.documentfile.provider.DocumentFile
 import com.anggrayudi.storage.file.child
 import com.anggrayudi.storage.file.getAbsolutePath
-import com.anggrayudi.storage.file.getFormattedSize
 import com.gee12.mytetroid.common.*
 import com.gee12.mytetroid.domain.provider.IStorageProvider
 import com.gee12.mytetroid.model.FilePath
@@ -33,12 +34,41 @@ class GetFolderSizeInStorageUseCase(
             if (!file.exists()) {
                 return Failure.File.NotExist(folderPath).toLeft()
             }
-            file.getFormattedSize(context).toRight()
+            val size = getFileSize(file)
+            Formatter.formatFileSize(context, size).toRight()
         } catch (ex: SecurityException) {
             Failure.File.AccessDenied(folderPath, ex).toLeft()
         } catch (ex: Exception) {
             Failure.File.GetFileSize(folderPath, ex).toLeft()
         }
+    }
+
+    private fun getFileSize(file: DocumentFile): Long {
+        if (!file.exists()) {
+            return 0
+        }
+        var size: Long = 0
+        if (!file.isDirectory) {
+            size = file.length()
+        } else {
+            val folders = mutableListOf<DocumentFile>()
+            folders.add(file)
+            while (folders.isNotEmpty()) {
+                val dir = folders.removeAt(0)
+                if (!dir.exists()) {
+                    continue
+                }
+                val listFiles = dir.listFiles()
+                if (listFiles.isEmpty()) {
+                    continue
+                }
+                for (child in listFiles) {
+                    size += child.length()
+                    if (child.isDirectory) folders.add(child)
+                }
+            }
+        }
+        return size
     }
 
 }
