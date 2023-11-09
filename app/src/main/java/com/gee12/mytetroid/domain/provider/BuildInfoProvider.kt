85c7c992eced91b59f2e0174c0f62a703d4c0d20
gee12 <yagee12@gmail.com>
2023-11-09 22:16:38 +0300

    Added 'googlePlay' buildVariant without MANAGE_EXTERNAL_STORAGE permission

--- app/src/main/java/com/gee12/mytetroid/domain/provider/BuildInfoProvider.kt
+++ app/src/main/java/com/gee12/mytetroid/domain/provider/BuildInfoProvider.kt
@@ -10,10 +10,10 @@ class BuildInfoProvider(context: Context) {
 
     val applicationId: String = BuildConfig.APPLICATION_ID
 
-    fun isFullVersion() = BuildConfig.FLAVOR == "pro" //|| BuildConfig.FLAVOR == "googlePlayPro"
-    fun isFreeVersion() = BuildConfig.FLAVOR == "free" //|| BuildConfig.FLAVOR == "googlePlayFree"
+    fun isFullVersion() = BuildConfig.FLAVOR == "pro" || BuildConfig.FLAVOR == "googlePlayPro"
+    fun isFreeVersion() = BuildConfig.FLAVOR == "free" || BuildConfig.FLAVOR == "googlePlayFree"
 
-    fun hasAllFilesAccessVersion() = false //BuildConfig.FLAVOR != "googlePlayPro" && BuildConfig.FLAVOR != "googlePlayFree"
+    fun hasAllFilesAccessVersion() = BuildConfig.FLAVOR != "googlePlayPro" && BuildConfig.FLAVOR != "googlePlayFree"
 
     val appUpdateTime: Long = context.packageManager.getPackageInfo(context.packageName, 0).lastUpdateTime
 
