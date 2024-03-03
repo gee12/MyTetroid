9b92912eb6d66df614d1212f2412246ff8ddc451
gee12 <yagee12@gmail.com>
2024-03-03 19:19:13 +0300

    Fixes in default scripts (2)

--- app/src/main/assets/scripts/link_url_tooltip.js
+++ app/src/main/assets/scripts/link_url_tooltip.js
@@ -19,7 +19,6 @@ function linkUrl_userScript() {
             '    color: #333; ' +
             '    background: #fff; ' +
             '    box-shadow: 3px 3px 3px rgba(0, 0, 0, .3); ' +
-
             '    overflow-wrap: break-word; ' +
             '    word-break: break-all;  ' +
             '} ';
