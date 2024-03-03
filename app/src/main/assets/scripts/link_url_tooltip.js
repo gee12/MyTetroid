1581ab174410e285a472998ab410e3eed2b3d75d
gee12 <yagee12@gmail.com>
2024-03-03 19:18:50 +0300

    Fixes in default scripts (2)

--- app/src/main/assets/scripts/link_url_tooltip.js
+++ app/src/main/assets/scripts/link_url_tooltip.js
@@ -1,97 +1,108 @@
-addStyle();
+linkUrl_userScript();
 
-var tooltipElem;
+var linkUrl_tooltipElem;
 
-function addStyle() {
-    const style = document.createElement("style");
-    style.innerHTML = '' +
-        '.tooltip { ' +
-        '    position: fixed; ' +
-        '    padding: 10px 20px; ' +
-        '    border: 1px solid #b3c9ce; ' +
-        '    border-radius: 4px; ' +
-        '    text-align: center; ' +
-        '    font: italic 14px/1.3 sans-serif; ' +
-        '    color: #333; ' +
-        '    background: #fff; ' +
-        '    box-shadow: 3px 3px 3px rgba(0, 0, 0, .3); ' +
+function linkUrl_userScript() {
 
-        '    overflow-wrap: break-word; ' +
-        '    word-break: break-all;  ' +
-        '} ';
-    document.head.appendChild(style);
-}
+    addStyle();
 
-function setLinksCallbacks() {
-    const links = document.querySelectorAll("a");
-    for (var i = 0; i < links.length; i++) {
-        setLinkCallbacks(links[i]);
-    }
-}
+    function addStyle() {
+        const style = document.createElement("style");
+        style.innerHTML = '' +
+            '.linkUrl_tooltip { ' +
+            '    position: fixed; ' +
+            '    padding: 10px 20px; ' +
+            '    border: 1px solid #b3c9ce; ' +
+            '    border-radius: 4px; ' +
+            '    text-align: center; ' +
+            '    font: italic 14px/1.3 sans-serif; ' +
+            '    color: #333; ' +
+            '    background: #fff; ' +
+            '    box-shadow: 3px 3px 3px rgba(0, 0, 0, .3); ' +
 
-function setLinkCallbacks(linkElem) {
-    // or onmouseover/click
-    linkElem.oncontextmenu = function(e) {
-        onShowTooltip(linkElem.href, e);
+            '    overflow-wrap: break-word; ' +
+            '    word-break: break-all;  ' +
+            '} ';
+        document.head.appendChild(style);
     }
 
-    linkElem.onmouseout = onRemoveTooltip;
-}
-
-function onShowTooltip(href, e) {
-     e.preventDefault();
-     RE.clearSelection();
-
-     const target = e.target;
-     const tooltipHtml = href;
-
-     tooltipElem = document.createElement("div");
-     tooltipElem.className = "tooltip";
-
-     const innerLinkElem = document.createElement("a");
-     innerLinkElem.href = tooltipHtml;
-     innerLinkElem.innerText = tooltipHtml;
-     tooltipElem.appendChild(innerLinkElem);
-
-     document.body.appendChild(tooltipElem);
-
-     const coords = target.getBoundingClientRect();
-     var left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
-     if (left < 0) left = 0;
-     var top = coords.top - tooltipElem.offsetHeight - 5;
-     if (top < 0) {
-         top = coords.top + target.offsetHeight + 5;
-     }
-     var maxWidth = RE.editor.offsetWidth - 26;
-
-     tooltipElem.style.left = left + 'px';
-     tooltipElem.style.top = top + 'px';
-     if (maxWidth > 0) {
-         tooltipElem.style.maxWidth = maxWidth + 'px';
-     }
-};
-
-const onRemoveTooltip = function(e) {
-    const elem = e.relatedTarget;
-    const divElem = (elem.nodeName.toLowerCase() == "div") ? elem : elem.parentNode;
-
-    if (tooltipElem && tooltipElem !== divElem) {
-        tooltipElem.remove();
-        tooltipElem = null;
-    } else if (divElem) {
-        divElem.onmouseout = function(e) {
-            divElem.remove();
+    function setLinksCallbacks() {
+        const links = document.querySelectorAll("a");
+        for (var i = 0; i < links.length; i++) {
+            setLinkCallbacks(links[i]);
         }
     }
-};
 
-RE.addOnBeforeSaveHtmlContentEventListener(function () {
-    const elements = document.getElementsByClassName("tooltip");
-    while (elements.length > 0) {
-        elements[0].parentNode.removeChild(elements[0]);
+    function setLinkCallbacks(linkElem) {
+        // or onmouseover/click
+        linkElem.oncontextmenu = function(e) {
+             e.preventDefault();
+             RE.clearSelection();
+
+            onShowTooltip(linkElem.href, e);
+        }
+
+        linkElem.onmouseout = onRemoveTooltip;
     }
-});
 
-RE.addOnAfterLoadHtmlContentEventListener(function () {
-    setLinksCallbacks();
-});
+    function onShowTooltip(href, e) {
+         const target = e.target;
+
+         const tooltipElem = document.createElement("div");
+         tooltipElem.className = "linkUrl_tooltip";
+
+         const innerLinkElem = document.createElement("a");
+         innerLinkElem.href = href;
+         innerLinkElem.innerText = href;
+         tooltipElem.appendChild(innerLinkElem);
+
+         document.body.appendChild(tooltipElem);
+
+         const coords = target.getBoundingClientRect();
+         var left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+         if (left < 0) left = 0;
+         var top = coords.top - tooltipElem.offsetHeight - 5;
+         if (top < 0) {
+             top = coords.top + target.offsetHeight + 5;
+         }
+         var maxWidth = RE.editor.offsetWidth - 26;
+
+         tooltipElem.style.left = left + 'px';
+         tooltipElem.style.top = top + 'px';
+         if (maxWidth > 0) {
+             tooltipElem.style.maxWidth = maxWidth + 'px';
+         }
+         linkUrl_tooltipElem = tooltipElem;
+    };
+
+    const onRemoveTooltip = function(e) {
+
+    // FIXME
+        const elem = e.relatedTarget;
+        const divElem = (elem.nodeName.toLowerCase() == "div") ? elem : elem.parentNode;
+
+        if (linkUrl_tooltipElem && linkUrl_tooltipElem !== divElem) {
+            linkUrl_tooltipElem.remove();
+            linkUrl_tooltipElem = null;
+        } else if (divElem) {
+            divElem.onmouseout = function(e) {
+                divElem.remove();
+            }
+        }
+    };
+
+    function removeTooltips() {
+        const elements = document.getElementsByClassName("linkUrl_tooltip");
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+    }
+
+    RE.addOnBeforeSaveHtmlContentEventListener(function () {
+        removeTooltips();
+    });
+
+    RE.addOnAfterLoadHtmlContentEventListener(function () {
+        setLinksCallbacks();
+    });
+}
--- app/src/main/assets/scripts/table_sort.js
+++ app/src/main/assets/scripts/table_sort.js
@@ -1,77 +1,83 @@
-function setTablesCallbacks() {
-    const tables = document.querySelectorAll("table");
-    for (var i = 0; i < tables.length; i++) {
-        setTableCallbacks(tables[i]);
-    }
-}
+tableSort_userScript();
 
-function setTableCallbacks(table) {
-    if (table.rows.length > 0) {
-        const cells = table.rows[0].cells;
-        for (var j = 0; j < cells.length; j++) {
-            setCellCallback(table, cells[j]);
+function tableSort_userScript() {
+
+    function setTablesCallbacks() {
+        const tables = document.querySelectorAll("table");
+        for (var i = 0; i < tables.length; i++) {
+            setTableCallbacks(tables[i]);
         }
     }
-}
 
-function setCellCallback(table, cell) {
-    cell.onclick = function(e) {
-        e.preventDefault();
-        RE.clearSelection();
-        sortTableByColumn(table, cell);
-    };
-}
-
-function sortTableByColumn(table, cell) {
-    RE.beforeTextChange();
-
-    const tbody = table.querySelector("tbody");
-    const parent = (tbody != null) ? tbody : table;
-    const rows = parent.querySelectorAll('tr:nth-child(n+2)');
-
-    const rowsArray = toArray(rows);
-    const compareFn = compareRowsFnFactory(cell.cellIndex, this.asc = !this.asc);
-    rowsArray.sort(compareFn);
-    for (var i = 0; i < rowsArray.length; i++) {
-        parent.appendChild(rowsArray[i]);
+    function setTableCallbacks(table) {
+        if (table.rows.length > 0) {
+            const cells = table.rows[0].cells;
+            for (var j = 0; j < cells.length; j++) {
+                setCellCallback(table, cells[j]);
+            }
+        }
     }
 
-    RE.textChange();
-}
+    function setCellCallback(table, cell) {
+        cell.onclick = function(e) {
+            e.preventDefault();
+            RE.clearSelection();
 
-function compareRowsFnFactory(colIndex, asc) {
-    return function(a, b) {
-        const cellValue1 = getCellValue(a, colIndex);
-        const cellValue2 = getCellValue(b, colIndex);
-        return asc
-            ? compareCellValues(cellValue1, cellValue2)
-            : compareCellValues(cellValue2, cellValue1);
-    };
-}
+            sortTableByColumn(table, cell);
+        };
+    }
 
-function getCellValue(row, colIndex) {
-    const cell = row.children[colIndex];
-    return cell.innerText || cell.textContent;
-}
+    function sortTableByColumn(table, cell) {
+        RE.beforeTextChange();
 
-function compareCellValues(cellValue1, cellValue2) {
-    return isEmptyOrNaN(cellValue1) || isEmptyOrNaN(cellValue2)
-        ? cellValue1.toString().localeCompare(cellValue2)
-        : cellValue1 - cellValue2;
-}
+        const tbody = table.querySelector("tbody");
+        const parent = (tbody != null) ? tbody : table;
+        const rows = parent.querySelectorAll('tr:nth-child(n+2)');
 
-function isEmptyOrNaN(value) {
-    return value === "" || isNaN(value);
-}
+        const rowsArray = toArray(rows);
+        const compareFn = compareRowsFnFactory(cell.cellIndex, this.asc = !this.asc);
+        rowsArray.sort(compareFn);
+        for (var i = 0; i < rowsArray.length; i++) {
+            parent.appendChild(rowsArray[i]);
+        }
 
-function toArray(nodesList) {
-	var arr = [];
-	for(var i = 0, node; node = nodesList[i]; ++i) {
-		arr.push(node);
-	}
-	return arr;
-}
+        RE.textChange();
+    }
 
-RE.addOnAfterLoadHtmlContentEventListener(function () {
-    setTablesCallbacks();
-});
+    function compareRowsFnFactory(colIndex, asc) {
+        return function(a, b) {
+            const cellValue1 = getCellValue(a, colIndex);
+            const cellValue2 = getCellValue(b, colIndex);
+            return asc
+                ? compareCellValues(cellValue1, cellValue2)
+                : compareCellValues(cellValue2, cellValue1);
+        };
+    }
+
+    function getCellValue(row, colIndex) {
+        const cell = row.children[colIndex];
+        return cell.innerText || cell.textContent;
+    }
+
+    function compareCellValues(cellValue1, cellValue2) {
+        return isEmptyOrNaN(cellValue1) || isEmptyOrNaN(cellValue2)
+            ? cellValue1.toString().localeCompare(cellValue2)
+            : cellValue1 - cellValue2;
+    }
+
+    function isEmptyOrNaN(value) {
+        return value === "" || isNaN(value);
+    }
+
+    function toArray(nodesList) {
+        var arr = [];
+        for(var i = 0, node; node = nodesList[i]; ++i) {
+            arr.push(node);
+        }
+        return arr;
+    }
+
+    RE.addOnAfterLoadHtmlContentEventListener(function () {
+        setTablesCallbacks();
+    });
+}
