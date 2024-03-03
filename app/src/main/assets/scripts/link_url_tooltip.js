dd4126c617c6c58caca25ca19b7d8cb39733552b
gee12 <yagee12@gmail.com>
2024-03-03 16:05:29 +0300

    Added default script 'link_url_tooltip.js'

---
+++ app/src/main/assets/scripts/link_url_tooltip.js
@@ -0,0 +1,96 @@
+addStyle();
+
+var tooltipElem;
+
+function addStyle() {
+    const style = document.createElement("style");
+    style.innerHTML = '' +
+        '.tooltip { ' +
+        '    position: fixed; ' +
+        '    padding: 10px 20px; ' +
+        '    border: 1px solid #b3c9ce; ' +
+        '    border-radius: 4px; ' +
+        '    text-align: center; ' +
+        '    font: italic 14px/1.3 sans-serif; ' +
+        '    color: #333; ' +
+        '    background: #fff; ' +
+        '    box-shadow: 3px 3px 3px rgba(0, 0, 0, .3); ' +
+
+        '    overflow-wrap: break-word; ' +
+        '    word-break: break-all;  ' +
+        '} ';
+    document.head.appendChild(style);
+}
+
+function setLinksCallbacks() {
+    const links = document.querySelectorAll("a");
+    for (var i = 0; i < links.length; i++) {
+        setLinkCallbacks(links[i]);
+    }
+}
+
+function setLinkCallbacks(linkElem) {
+    const event = 'contextmenu'; // onmouseover/click
+    linkElem.removeEventListener(event, onShowTooltip);
+    linkElem.addEventListener(event, onShowTooltip);
+
+    linkElem.onmouseout = onRemoveTooltip;
+}
+
+const onShowTooltip = function(e) {
+     e.preventDefault();
+     RE.clearSelection();
+
+     const target = e.target;
+     const tooltipHtml = target.href;
+
+     tooltipElem = document.createElement("div");
+     tooltipElem.className = "tooltip";
+
+     const innerLinkElem = document.createElement("a");
+     innerLinkElem.href = tooltipHtml;
+     innerLinkElem.innerText = tooltipHtml;
+     tooltipElem.appendChild(innerLinkElem);
+
+     document.body.appendChild(tooltipElem);
+
+     const coords = target.getBoundingClientRect();
+     var left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+     if (left < 0) left = 0;
+     var top = coords.top - tooltipElem.offsetHeight - 5;
+     if (top < 0) {
+         top = coords.top + target.offsetHeight + 5;
+     }
+     var maxWidth = RE.editor.offsetWidth - 26;
+
+     tooltipElem.style.left = left + 'px';
+     tooltipElem.style.top = top + 'px';
+     if (maxWidth > 0) {
+         tooltipElem.style.maxWidth = maxWidth + 'px';
+     }
+};
+
+const onRemoveTooltip = function(e) {
+    const elem = e.relatedTarget;
+    const divElem = (elem.nodeName.toLowerCase() == "div") ? elem : elem.parentNode;
+
+    if (tooltipElem && tooltipElem !== divElem) {
+        tooltipElem.remove();
+        tooltipElem = null;
+    } else if (divElem) {
+        divElem.onmouseout = function(e) {
+            divElem.remove();
+        }
+    }
+};
+
+RE.addOnBeforeSaveHtmlContentEventListener(function () {
+    const elements = document.getElementsByClassName("tooltip");
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+});
+
+RE.addOnAfterLoadHtmlContentEventListener(function () {
+    setLinksCallbacks();
+});
--- app/src/main/java/com/gee12/mytetroid/model/enums/DefaultScript.kt
+++ app/src/main/java/com/gee12/mytetroid/model/enums/DefaultScript.kt
@@ -5,13 +5,15 @@ import com.gee12.mytetroid.domain.provider.IResourcesProvider
 
 enum class DefaultScript(val fileName: String) {
     TableSort("table_sort.js"),
-    TableTotals("table_totals.js");
+    TableTotals("table_totals.js"),
+    LinkUrl("link_url_tooltip.js");
 
     fun getTitle(resourcesProvider: IResourcesProvider): String {
         return resourcesProvider.getString(
             when (this) {
                 TableSort -> R.string.title_def_script_title_table_sort
                 TableTotals -> R.string.title_def_script_title_table_totals
+                LinkUrl -> R.string.title_def_script_title_link_url
             }
         )
     }
@@ -21,6 +23,7 @@ enum class DefaultScript(val fileName: String) {
             when (this) {
                 TableSort -> R.string.title_def_script_description_table_sort
                 TableTotals -> R.string.title_def_script_description_table_totals
+                LinkUrl -> R.string.title_def_script_description_link_url
             }
         )
     }
